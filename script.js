4325b7667c55841f78f4a77745da6ace7b1b13e0
Raiyan Jamal <rayn.jamal@gmail.com>
2018-02-13 19:03:15 +0600

    added a function for random orange color stop

--- script.js
+++ script.js
@@ -9,14 +9,24 @@ function getRandomColor() {
 }
 // this function gives us a random RED color
 function getRandomRed() {
-    var reds = ["50, 20, 20", "179, 27, 27", "128, 0, 0", "206, 22, 32", "169, 0, 0", "227, 66, 52", "131, 0, 0", "60, 20, 20", "255, 0, 0", "112, 28, 28", "204, 0, 0", "178, 34, 34", "164, 0, 0", "195, 0, 0", "255, 8, 0", "67, 0, 0", "215, 59, 62", "208, 0, 0", "139, 0, 0"];
+    var reds = ["179, 27, 27", "128, 0, 0", "206, 22, 32", "169, 0, 0", "227, 66, 52", "131, 0, 0", "60, 20, 20", "255, 0, 0", "112, 28, 28", "204, 0, 0", "178, 34, 34", "164, 0, 0", "195, 0, 0", "255, 8, 0", "67, 0, 0", "215, 59, 62", "208, 0, 0", "139, 0, 0"];
+    var oranges = ["255, 229, 6", "255, 179, 71", "255, 168, 18", "255, 167, 0", "255, 140, 0", "255, 127, 0", "255, 117, 24", "255, 103, 0", "204, 85, 0", "255, 191, 0", "255, 219, 88", "244, 196, 48", "255, 255, 0"];
 
-    var getReds = reds[Math.floor(Math.random() * 19)];
+    var getReds = reds[Math.floor(Math.random() * 18)];
+    var getOranges = oranges[Math.floor(Math.random() * 13)];
 
-    var outputRGB = "rgb(" + getReds + ")";
-    return outputRGB;
+    var outputReds = "rgb(" + getReds + ")";
+    return outputReds;
 }
-console.log(getRandomRed());
+// this function gives us a random RED color
+function getRandomOrange() {
+    var oranges = ["255, 229, 6", "255, 179, 71", "255, 168, 18", "255, 167, 0", "255, 140, 0", "255, 127, 0", "255, 117, 24", "255, 103, 0", "204, 85, 0", "255, 191, 0", "255, 219, 88", "244, 196, 48", "255, 255, 0"];
+
+    var getOranges = oranges[Math.floor(Math.random() * 13)];
+
+    var outputOranges = "rgb(" + getOranges + ")";
+    return outputOranges;
+}
 
 // this function gives us a random direction
 function getRandomAngle() {
@@ -27,15 +37,13 @@ function getRandomAngle() {
 // this function prepare the linear gradient,
 // outputs gradient & background code
 function setRandomColor() {
-    var inAngle = getRandomAngle();
     var thePad = document.getElementById('color-pad').style.background = 'linear-gradient(' +
-        inAngle + 'deg' + ', ' + getRandomColor() + ' 0%, ' + getRandomColor() + ' 100%)';
+        getRandomAngle() + 'deg' + ', ' + getRandomColor() + ' 0%, ' + getRandomColor() + ' 100%)';
     document.getElementById("gradient-output").innerHTML = thePad;
 }
 
 function setRandomRed() {
-    var inAngle = getRandomAngle();
     var thePad2 = document.getElementById('color-pad2').style.background = 'linear-gradient(' +
-        inAngle + 'deg' + ', ' + getRandomRed() + ' 0%, ' + "rgb(239, 137, 6)" + ' 100%)';
+        getRandomAngle() + 'deg' + ', ' + getRandomRed() + ' 0%, ' + getRandomOrange() + ' 100%)';
     document.getElementById("gradient-output2").innerHTML = thePad2;
 }
