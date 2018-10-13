ee51e199d1333e7496a8536e65a3a9d0929fb402
Mohammed Raiyan Jamal <rayn.jamal@gmail.com>
2018-10-13 12:17:54 +0200

    changed all the es5 functions to Arrow function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // this function gives us a random color-stop
-function getRandomColor() {
+getRandomColor() => {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
@@ -8,7 +8,7 @@ function getRandomColor() {
     return color;
 }
 // this function gives us a random RED color
-function getRandomRed() {
+getRandomRed() => {
     var reds = ["179, 27, 27", "128, 0, 0", "206, 22, 32", "169, 0, 0", "227, 66, 52", "131, 0, 0", "60, 20, 20", "255, 0, 0", "112, 28, 28", "204, 0, 0", "178, 34, 34", "164, 0, 0", "195, 0, 0", "255, 8, 0", "67, 0, 0", "215, 59, 62", "208, 0, 0", "139, 0, 0"];
     var oranges = ["255, 229, 6", "255, 179, 71", "255, 168, 18", "255, 167, 0", "255, 140, 0", "255, 127, 0", "255, 117, 24", "255, 103, 0", "204, 85, 0", "255, 191, 0", "255, 219, 88", "244, 196, 48", "255, 255, 0"];
 
@@ -19,7 +19,7 @@ function getRandomRed() {
     return outputReds;
 }
 // this function gives us a random RED color
-function getRandomOrange() {
+getRandomOrange() => {
     var oranges = ["255, 229, 6", "255, 179, 71", "255, 168, 18", "255, 167, 0", "255, 140, 0", "255, 127, 0", "255, 117, 24", "255, 103, 0", "204, 85, 0", "255, 191, 0", "255, 219, 88", "244, 196, 48", "255, 255, 0"];
 
     var getOranges = oranges[Math.floor(Math.random() * 13)];
@@ -29,20 +29,20 @@ function getRandomOrange() {
 }
 
 // this function gives us a random direction
-function getRandomAngle() {
+getRandomAngle() => {
     var angle = 4.04;
     angle *= Math.floor(Math.random() * 90);
     return Math.floor(angle);
 }
 // this function prepare the linear gradient,
 // outputs gradient & background code
-function setRandomColor() {
+setRandomColor() => {
     var thePad = document.getElementById('color-pad').style.background = 'linear-gradient(' +
         getRandomAngle() + 'deg' + ', ' + getRandomColor() + ' 0%, ' + getRandomColor() + ' 100%)';
     document.getElementById("gradient-output").innerHTML = thePad;
 }
 
-function setRandomRed() {
+setRandomRed() => {
     var thePad2 = document.getElementById('color-pad2').style.background = 'linear-gradient(' +
         getRandomAngle() + 'deg' + ', ' + getRandomRed() + ' 0%, ' + getRandomOrange() + ' 100%)';
     document.getElementById("gradient-output2").innerHTML = thePad2;
