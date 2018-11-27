be18d2c17d96bff215382d630755bd51ddd0bc92
raiyanjamal <rayn.jamal@gmail.com>
2018-11-27 22:21:55 +0100

    cleaned up the codebase

--- script.js
+++ script.js
@@ -7,41 +7,27 @@ let getRandomColor = () => {
     }
     return color;
 }
-// this function gives us a random RED color
-// let getRandomRed = () => {
-//     let reds = ["179, 27, 27", "128, 0, 0", "206, 22, 32", "169, 0, 0", "227, 66, 52", "131, 0, 0", "60, 20, 20", "255, 0, 0", "112, 28, 28", "204, 0, 0", "178, 34, 34", "164, 0, 0", "195, 0, 0", "255, 8, 0", "67, 0, 0", "215, 59, 62", "208, 0, 0", "139, 0, 0"];
-//     let oranges = ["255, 229, 6", "255, 179, 71", "255, 168, 18", "255, 167, 0", "255, 140, 0", "255, 127, 0", "255, 117, 24", "255, 103, 0", "204, 85, 0", "255, 191, 0", "255, 219, 88", "244, 196, 48", "255, 255, 0"];
 
-//     let getReds = reds[Math.floor(Math.random() * 18)];
-//     let getOranges = oranges[Math.floor(Math.random() * 13)];
-
-//     let outputReds = "rgb(" + getReds + ")";
-//     return outputReds;
-// }
-
-// this function gives us a random RED color
-// let getRandomOrange = () => {
-//     let oranges = ["255, 229, 6", "255, 179, 71", "255, 168, 18", "255, 167, 0", "255, 140, 0", "255, 127, 0", "255, 117, 24", "255, 103, 0", "204, 85, 0", "255, 191, 0", "255, 219, 88", "244, 196, 48", "255, 255, 0"];
-
-//     let getOranges = oranges[Math.floor(Math.random() * 13)];
-
-//     let outputOranges = "rgb(" + getOranges + ")";
-//     return outputOranges;
-// }
-
-// this function gives us a random direction
+// this function gives us a random angle in deg
 let getRandomAngle = () => {
     let angle = 4.04;
     angle *= Math.floor(Math.random() * 90);
     return Math.floor(angle);
 }
 
-// this function prepare the linear gradient,
-// outputs gradient & background code
-let setRandomColor = () => {
-    let thePad = document.getElementById('color-pad').style.background = `linear-gradient(${getRandomAngle()}deg, ${getRandomColor()} 0%, ${getRandomColor()} 100%)`;
+// finally, generates the linear-gradient
+let getRandomGradient = () => {
+    return `linear-gradient(${getRandomAngle()}deg, ${getRandomColor()} 0%, ${getRandomColor()} 100%)`;
+}
 
-    document.getElementById("gradient-output").innerHTML = thePad;
+// here on click, it prepares the linear gradient,
+// outputs the gradient & also background code
+document.getElementById('sub-btn').addEventListener("click", () => {
+    // scroll to color-pad smoothly
     let elmnt = document.getElementById("color-pad");
     elmnt.scrollIntoView({ behavior: 'smooth' });
-}
+    // updates css, brackground
+    let thePad = document.getElementById('color-pad').style.background = getRandomGradient();
+    // updates the output view
+    document.getElementById("gradient-output").innerHTML = thePad;
+})
